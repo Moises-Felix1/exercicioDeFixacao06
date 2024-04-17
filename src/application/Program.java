e016c658679d861b7c46cef9eb1a5e0d7ae9db1a
Moises Felix <moises012felix@gmail.com>
2024-04-17 19:37:14 -0300

    Exercicio finalizado

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,46 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
+
+import entities.Students;
+
 public class Program {
 
 	public static void main(String[] args) {
 
-		System.out.println("Iniciando");
+		Scanner sc = new Scanner(System.in);
+		try {
+			List<String> courses = new ArrayList<>();
+			courses.add("A");
+			courses.add("B");
+			courses.add("C");
+			
+			Set<Students> std = new HashSet<>();
+			for(String crss : courses) {
+				
+				System.out.print("How many students for course " +crss+"? ");
+				int quanti = sc.nextInt();
+				
+				for(int i=0; i<quanti; i++) {
+					
+					int id = sc.nextInt();
+					std.add(new Students(id));
+				}
+				System.out.println();
+			}
+			
+			System.out.print("Total students: "+std.size());
+			
+		}catch(InputMismatchException e) {
+			System.out.println("Error! "+e.getMessage());
+		}finally {
+			sc.close();
+		}
 	}
 
 }
